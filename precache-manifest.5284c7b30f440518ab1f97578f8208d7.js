d9eabdc62f8c648561cdfe8465e5da2e7bb759db
pius <piuskariuki19@gmail.com>
2020-05-23 22:14:00 +0300

    Updates

---
+++ precache-manifest.5284c7b30f440518ab1f97578f8208d7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3567736a5db02051f1b470c98a8ea683",
+    "url": "/://PiusKariuki.github.io/to-to-app/index.html"
+  },
+  {
+    "revision": "220cc97ca9b2e8b15309",
+    "url": "/://PiusKariuki.github.io/to-to-app/static/css/main.1a97ceb2.chunk.css"
+  },
+  {
+    "revision": "36e12e41d02c32b8d909",
+    "url": "/://PiusKariuki.github.io/to-to-app/static/js/2.c7110b6e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/://PiusKariuki.github.io/to-to-app/static/js/2.c7110b6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "220cc97ca9b2e8b15309",
+    "url": "/://PiusKariuki.github.io/to-to-app/static/js/main.c1e559e3.chunk.js"
+  },
+  {
+    "revision": "a4c941fb0148dbc29b37",
+    "url": "/://PiusKariuki.github.io/to-to-app/static/js/runtime-main.40c12a83.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/://PiusKariuki.github.io/to-to-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
