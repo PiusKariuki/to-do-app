33a0ec1c90c91594df537612bbdd45a290992e39
pius <piuskariuki19@gmail.com>
2020-05-23 22:39:31 +0300

    Updates

---
+++ precache-manifest.9b6df045e76328840f89e4db96624209.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a4ce1bd2f9763fac8b424634226e5ca",
+    "url": "/to-do-app/index.html"
+  },
+  {
+    "revision": "4259f3a188b4f27ca18a",
+    "url": "/to-do-app/static/css/main.cfb5a67d.chunk.css"
+  },
+  {
+    "revision": "b62b8a7071e68b270122",
+    "url": "/to-do-app/static/js/2.87eb8b5d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/to-do-app/static/js/2.87eb8b5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4259f3a188b4f27ca18a",
+    "url": "/to-do-app/static/js/main.6bf9d9aa.chunk.js"
+  },
+  {
+    "revision": "2b45b91da13e80bbc4fc",
+    "url": "/to-do-app/static/js/runtime-main.a952dbe5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/to-do-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/://PiusKariuki.github.io/to-to-app/precache-manifest.5284c7b30f440518ab1f97578f8208d7.js"
+  "/to-do-app/precache-manifest.9b6df045e76328840f89e4db96624209.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/://PiusKariuki.github.io/to-to-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/to-do-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
